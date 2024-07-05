53a3ad8869ff24ff7bcc1bf70a48d8d92fb05df1
SahithyaMaragatham <102693647+SahithyaMaragatham@users.noreply.github.com>
2024-07-06 01:40:41 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,39 @@
 const { Sequelize } = require("sequelize");
 const config = require("./config.json")[process.env.NODE_ENV || "development"];
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  {
-    host: config.host,
-    dialect: config.dialect,
-  }
-);
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+      host: config.host,
+      dialect: config.dialect,
+      logging: false
+    }
+  );
+}
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
