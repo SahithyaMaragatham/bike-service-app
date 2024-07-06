8cfd5c9e34ed53224eb63e094bcc4aaa112a07e6
SahithyaMaragatham <da.sahithyamaragatham@gmail.com>
2024-07-06 23:20:51 +0530

    updated cors orgin

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bike-service-app000.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
