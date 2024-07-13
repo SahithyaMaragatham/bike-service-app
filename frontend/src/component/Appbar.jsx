aee20a60a47ef63116a37b470523b999b86439b2
SahithyaMaragatham <102693647+SahithyaMaragatham@users.noreply.github.com>
2024-07-13 15:06:24 +0530

    Update Appbar.jsx

--- frontend/src/component/Appbar.jsx
+++ frontend/src/component/Appbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 
 const Appbar = () => {
   const navigate = useNavigate();
@@ -7,12 +8,23 @@ const Appbar = () => {
   const handleHome = () => {
     navigate("/");
   };
+
   return (
-    <>
-      <div>
-        <button onClick={handleHome}>Home</button>
-      </div>
-    </>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand onClick={handleHome} style={{ cursor: "pointer" }}>
+          Bike Service App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Button variant="outline-light" onClick={handleHome}>
+              Home
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
